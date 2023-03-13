5208f56c0a7aa93d8ead5d4089a6da4ba6eeb3e5
willrockies <wlsn.ads@gmail.com>
2023-03-13 19:13:12 -0300

    Adicionar arquivos de projeto.

---
+++ FilmeApi/Controllers/FilmeController.cs
@@ -0,0 +1,20 @@
+﻿using FilmeApi.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FilmeApi.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class FilmeController : ControllerBase
+{
+    private static List<Filme> filmes = new List<Filme>();
+
+    [HttpPost]
+    public void AdicionaFilme([FromBody]Filme filme)
+    {
+
+        filmes.Add(filme);
+        Console.WriteLine(filme.Titulo);
+        Console.WriteLine(filme.Duracao);
+    }
+}
---
+++ FilmeApi/Models/Filme.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FilmeApi.Models;
+
+public class Filme
+{
+    [Required(ErrorMessage = "O Título do filme é obrigatiório")]
+    public string Titulo { get; set; }
+    [Required (ErrorMessage = "O Gênerodo filme é obrigatiório")]
+    [MaxLength(50, ErrorMessage = "O tamanho do gênero não pode exceder 50 caracteres")]
+    public string Genero { get; set; }
+    [Required]
+    [Range(70,600, ErrorMessage ="A duração deve ter entre 70 e 600 minutos")]
+    public int Duracao { get; set; }
+}
